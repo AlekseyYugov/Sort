9b894fd458feadc8223ca14f5f215fb26acc61f8
Alex <aleksey.yugov.1992@gmail.com>
2021-11-25 17:22:02 +0500

    Сортировка выбором

---
+++ Algorithm_Selection_Sort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Algorithm_Selection_Sort
+{
+    class Program
+    {
+
+        static int IndexOfMin(int[]array, int n)
+        {
+            int result = n;
+            for (int i = n; i < array.Length; i++)
+            {
+                if (array[i]<array[result])
+                {
+                    result = i;
+                }
+            }
+            return result;
+        }
+        static void Swap(ref int element1, ref int element2)
+        {
+            var temp = element1;
+            element1 = element2;
+            element2 = temp;
+        }
+        static int[] SelectionSort(int[]array, int currentIndex = 0)
+        {
+            if (currentIndex == array.Length)
+            {
+                return array;
+            }
+            var index = IndexOfMin(array, currentIndex);
+            if (index!=currentIndex)
+            {
+                Swap(ref array[index], ref array[currentIndex]);
+            }
+            return SelectionSort(array, currentIndex + 1);
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(100);
+            }
+            Console.WriteLine("-----Сортировка выбором-----");
+            Console.WriteLine(string.Join(",", SelectionSort(array)));
+        }
+    }
+}
